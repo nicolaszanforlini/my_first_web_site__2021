42270d41bd563df11b21d39088fe645997d7099d
nicolaszanforlini <nicolas.zanforlini1@gmail.com>
2021-11-26 16:33:56 +0100

    rrr

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 
-/*exercice manipulation javascript*/
-function setForm() {
-  var objet = document.getElementById('objet').value;
-  var message = document.getElementById('message').value;
-  console.log(objet);
-  console.log(message);
-  alert(objet);
-  alert(message);
+
+/*test avec une class*/
+class Test {
+
+  constructor() {
+    this.objet = "";
+    this.message = "";
+  }
+
+  setForm() {
+    this.objet = document.getElementById('objet').value;
+    this.message = document.getElementById('message').value;
+    console.log(this.objet);
+    console.log(this.message);
+    }
 }
+
+
+
+let monTest = new Test();

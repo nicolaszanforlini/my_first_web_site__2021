0c9a4275343887923185ddfbffb871387269ce88
nicolaszanforlini <nicolas.zanforlini1@gmail.com>
2021-11-26 15:34:07 +0100

    cinq

--- js/script.js
+++ js/script.js
@@ -1,34 +1,10 @@
 
-
-class User {
-
-  constructor( login, password, email ) {
-    this.setPseudo(login);
-    this.setPassword(password);
-    this.setEmail(email);
-  }
-  setPseudo(pseudo) {
-    this.login = pseudo;
-  }
-  setPassword(pass) {
-    this.password = password;
-  }
-  setEmail(email) {
-    this.email = email;
-  }
-  getPseudo() {
-    return this.pseudo;
-  }
-  getPassword() {
-    return this.password;
-  }
-  getEmail() {
-    return this.email;
-  }
-
-  display_infos() {
-    console.log(this.getPseudo(),this.getPassword(),this.getEmail());
-  }
-
-
+/*exercice manipulation javascript*/
+function setForm() {
+  var objet = document.getElementById('objet').value;
+  var message = document.getElementById('message').value;
+  console.log(objet);
+  console.log(message);
+  alert(objet);
+  alert(message);
 }
